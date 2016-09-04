4aa9c9503906e470750bf1810608f0f515f83b46
Andrew54757 <andrews54757@gmail.com>
2016-09-03 21:17:45 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Note that there is no license, you are free to use this as you wish
+// Also note that some are from my programming book
+
+module.exports = {
+toUrl: function(a) { // converts spaces to %20
+var spaceCount = 0, index, i = 0;
+for (i = 0; i < a.length; i++) {
+
+}
+
+}
+}
