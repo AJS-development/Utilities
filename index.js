388ad76dc4a420252131b833d4d2e56e0eb67838
Andrew54757 <andrews54757@gmail.com>
2016-09-03 21:21:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,5 @@
 // Also note that some are from my programming book
 
 module.exports = {
-toUrl: function(a) { // converts spaces to %20
-var spaceCount = 0, index, i = 0;
-for (i = 0; i < a.length; i++) {
 
-}
-
-}
 }
