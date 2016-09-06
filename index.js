73d5119626cb7c4a752f2cbd2f6c2cf476b8e6e5
Andrew54757 <andrews54757@gmail.com>
2016-09-06 12:07:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Note that there is no license, you are free to use this as you wish
-// Also note that some are from my programming book
 
 module.exports = {
+seed(seed,t) {
+  var t = (t) ? t : "iehwfyqdhihdihe3jd";
+  var cipher = crypto.createCipher('aes192',seed)
+var result = cipher.update(t,'utf8','hex')
+  result += cipher.final('hex');
+return result;
+}
 
 }
